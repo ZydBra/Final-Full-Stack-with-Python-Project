addd3a8de181c98441ddcc2647f56b742b1fbe4e
ZydBra <z.brazionis@gmail.com>
2023-01-19 12:55:02 +0200

    Padaryti slaptažodžio atstatymo template'ai

--- uzrasine/urls.py
+++ uzrasine/urls.py
@@ -22,6 +22,10 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('authentication.urls')),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('application.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
